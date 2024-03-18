011583e3d0bef5f3f1caa8836e26f32b289b6631
thomaswillix <thomaswfds@gmail.com>
2024-03-18 23:09:25 +0100

    Users Trying to implement user validation

--- src/client/Client.java
+++ src/client/Client.java
@@ -10,64 +10,85 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client extends Application {
+
     private static final DatagramSocket socket;
 
     static {
         try {
-            socket = new DatagramSocket();
-        } catch (SocketException e){
-            throw new RuntimeException();
+            socket = new DatagramSocket(); // init to any available port
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
         }
     }
 
     private static final InetAddress address;
 
-    static{
+    static {
         try {
             address = InetAddress.getByName("localhost");
-        } catch (UnknownHostException e){
-            throw new RuntimeException();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private static final String identifier = "Thomas";
+    private static String identifier;
 
-    private static final int SERVER_PORT = 8000; //send to server
+    private static final int SERVER_PORT = 8000; // send to server
 
     private static final TextArea messageArea = new TextArea();
+
     private static final TextField inputBox = new TextField();
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc  = new Scanner(System.in);
+        identifier = sc.nextLine();
+
         // thread for receiving messages
         ClientThread clientThread = new ClientThread(socket, messageArea);
         clientThread.start();
 
-        //send initialization message to the server
-        byte[] uid = ("init;" + identifier).getBytes();
-        DatagramPacket initialize = new DatagramPacket(uid, uid.length, address, SERVER_PORT);
+        // send initialization message to the server
+        String error = "Ese usuario no está displonible, introduzca uno nuevo";
+        byte[] errorBytes = error.getBytes();
 
-        launch();
+        /*DatagramPacket packet = new DatagramPacket(errorBytes, errorBytes.length);
+        do{
+            byte[] uuid = ("init; " + identifier).getBytes();
+            DatagramPacket initialize = new DatagramPacket(uuid, uuid.length, address, SERVER_PORT);
+            socket.send(initialize);
+        }
+        while (socket.receive(packet.getData()) != );*/
+
+        launch(); // launch GUI
+
     }
+
+
     @Override
     public void start(Stage primaryStage) {
+
         messageArea.setMaxWidth(500);
         messageArea.setEditable(false);
 
+
         inputBox.setMaxWidth(500);
         inputBox.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.ENTER){
-                String temp = identifier + ";" + inputBox.getText(); //message to send
-                messageArea.setText(messageArea.getText() + inputBox.getText() + "\n");// update messages on send
+            if (event.getCode() == KeyCode.ENTER) {
+                String temp = identifier + ";" + inputBox.getText(); // message to send
+                messageArea.setText(messageArea.getText() + inputBox.getText() + "\n"); // update messages on screen
                 byte[] msg = temp.getBytes(); // convert to bytes
-                inputBox.setText(""); //remove text from input box
+                inputBox.setText(""); // remove text from input box
 
-                //create a packet & send
+                // create a packet & send
                 DatagramPacket send = new DatagramPacket(msg, msg.length, address, SERVER_PORT);
                 try {
                     socket.send(send);
-                } catch (IOException e){
+                } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             }
--- src/client/ClientThread.java
+++ src/client/ClientThread.java
@@ -1,18 +1,21 @@
 package client;
 
-
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
-public class ClientThread extends Thread{
+public class ClientThread extends Thread {
+
     private DatagramSocket socket;
-    private byte[] buffer = new byte[256];
+    private byte[] incoming = new byte[256];
+
     private TextArea textArea;
 
-    public ClientThread(DatagramSocket socket, TextArea textArea){
+    public ClientThread(DatagramSocket socket, TextArea textArea) {
         this.socket = socket;
         this.textArea = textArea;
     }
@@ -20,16 +23,16 @@ public class ClientThread extends Thread{
     @Override
     public void run() {
         System.out.println("starting thread");
-        while (true){
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        while (true) {
+            DatagramPacket packet = new DatagramPacket(incoming, incoming.length);
             try {
                 socket.receive(packet);
-            } catch (IOException e){
-                throw new RuntimeException();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
             String message = new String(packet.getData(), 0, packet.getLength()) + "\n";
             String current = textArea.getText();
-            textArea.setText(current+message);
+            textArea.setText(current + message);
         }
     }
 }
